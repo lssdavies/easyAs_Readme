72716cb337c60ddec121a94725e9e4c6348a69bf
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-01-28 15:54:50 -0800

    cloned starter code and  removed cloned repo.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-//Importing  
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+//Importing modules needed for the app. 
 const fs = require('fs');
 const inquirer = require('inquirer');
 
