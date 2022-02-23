be8b6732e7efefb1cf7ff9b6717089296e0815af
lssdavies <91108369+IslandBoyL@users.noreply.github.com>
2022-02-22 18:06:07 -0800

    Corrected questions and generated a readme using the app

---
+++ README.md
@@ -0,0 +1,55 @@
+# EasyAS_Readme
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Description
+
+As a Developer I wanted to develop a README.md generator so I could quickly generate a professional README.
+I was motivated to automate the process of creating a README file for every project.
+I built this app to reduce the amount of time needed to create a professional README file.
+This app eliminates the tedious task of having to create a professional README from scratch. easyAS_Readme captures user input using prompts to develop a profession README.
+
+## Technologies Used
+
+JavaScript,Node
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+
+## Installation
+
+To install this project follow the  instructions below:
+
+Clone repository and then install dependencies, npm i
+
+
+## Usage
+
+Once the repository has been cloned and the user run npm install to download all the app dependencies, they have to fun the command node index.js in the comman line interface / terminal.
+
+## License
+    
+    This project is using the MIT License
+    
+  
+## Contributing
+
+Please feel free to contact me via email or on Github directly if you will like to contribute to this project.
+
+## Tests
+
+
+    This project does not include any test
+
+
+## Questions
+
+If you have any questions about this project or questions about anything in this repository, please feel free to contact me directly via email at: lssdavies@email.com. 
+
+You can contact me directly through github at [lssdavies](https://github.com/lssdavies/)
+
--- index.js
+++ index.js
@@ -1,141 +1,142 @@
 // TODO: Include packages needed for this application
 //Importing modules needed for the app.
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/generateMarkdown')
-const path = require('path');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
 //this caputers the users initial data name, github and email*/
 const readMeQuestions = [
   {
-    type: 'input',
-    name: 'name',
-    message: 'What is your name?',
+    type: "input",
+    name: "name",
+    message: "What is your name?",
     validate: (nameInput) => {
       if (nameInput) {
         return true;
       } else {
-        console.log('Please enter your name!');
+        console.log("Please enter your name!");
         return false;
       }
     },
   },
   {
-    type: 'input',
-    name: 'github',
-    message: 'What is your GitHub Username?',
+    type: "input",
+    name: "github",
+    message: "What is your GitHub Username?",
     validate: (githubInput) => {
       if (githubInput) {
         return true;
       } else {
-        console.log('Please enter your Github Username!');
+        console.log("Please enter your Github Username!");
         return false;
       }
     },
   },
   {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?',
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
     validate: (emailInput) => {
       if (emailInput) {
         return true;
       } else {
-        console.log('Please enter your Github Username!');
+        console.log("Please enter your Github Username!");
         return false;
       }
     },
   },
   {
-    type: 'input',
-    name: 'title',
-    message: 'What is the name of your project',
+    type: "input",
+    name: "title",
+    message: "What is the name of your project",
     validate: (titleInput) => {
       if (titleInput) {
         return true;
       } else {
-        console.log('Please enter a project name!');
+        console.log("Please enter a project name!");
         return false;
       }
     },
   },
   {
-    type: 'input',
-    name: 'description',
-    message: 'Briefly describe your project',
+    type: "input",
+    name: "description",
+    message: "Briefly describe your project",
     validate: (descriptionInput) => {
       if (descriptionInput) {
         return true;
       } else {
-        console.log('Please enter a brief description of your project!');
+        console.log("Please enter a brief description of your project!");
         return false;
       }
     },
   },
   {
-    type: 'input',
-    name: 'additional_0',
-    message: 'What was your motivation? (optional)',
+    type: "input",
+    name: "additional_0",
+    message: "What was your motivation? (optional)",
   },
   {
-    type: 'input',
-    name: 'additional_1',
-    message: 'Why did you build this project? (optional)',
+    type: "input",
+    name: "additional_1",
+    message: "Why did you build this project? (optional)",
   },
   {
-    type: 'input',
-    name: 'additional_2',
-    message: 'What problem does it solve? (optional)',
+    type: "input",
+    name: "additional_2",
+    message: "What problem does it solve? (optional)",
   },
   {
-    type: 'checkbox',
-    name: 'technologies',
-    message: 'What technolgies did you implement in your project?',
-    choices: ['HTML', 'CSS', 'JavaScript', 'Node', 'Express', 'SQL'],
+    type: "checkbox",
+    name: "technologies",
+    message: "What technolgies did you implement in your project?",
+    choices: ["HTML", "CSS", "JavaScript", "Node", "Express", "SQL"],
   },
   {
-    type: 'input',
-    name: 'installation',
-    message: 'What does a user have to do to use/run projecT?',
-    default: 'Clone repository and then install dependencies, npm i',
+    type: "input",
+    name: "installation",
+    message: "What does a user have to do to use/run project?",
+    default: "Clone repository and then install dependencies, npm i",
   },
-  
+
   {
-    type: 'input',
-    name: 'usage',
-    message: 'Please provide instructions on for using this project.',
-  },
-  {
-    type: 'checkbox',
-    name: 'license',
-    message: 'What kind of license does your project have?',
-    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    type: "input",
+    name: "usage",
+    message: "Please provide instructions on for using this project.",
   },
   {
-    type: 'confirm',
-    name: 'test',
-    message: 'Did you write a test for your application?',
+    type: "checkbox",
+    name: "license",
+    message: "What kind of license does your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"],
+  },
+  {
+    type: "confirm",
+    name: "test",
+    message: "Did you write a test for your application?",
     default: false,
   },
   {
-    type: 'input',
-    name: 'runTest',
-    message: 'What is the test command to test the app?',
-    default: 'npm test',
+    type: "input",
+    name: "runTest",
+    message: "What is the test command to test the app?",
+    default: "npm test",
     /*using when property to check if user will like to add a test to the app by looking at the response from test*/
-     when: ({ test }) => {
-        if (test) {
-          return true;
-        } else {
-          return false;
-        }
-      },
+    when: ({ test }) => {
+      if (test) {
+        return true;
+      } else {
+        return false;
+      }
+    },
   },
   {
-    type: 'input',
-    name: 'contributing',
-    message: 'Please advise on how you will like users to contribute to the repo?',
+    type: "input",
+    name: "contributing",
+    message:
+      "Please advise on how you will like users to contribute to the repo?",
   },
 ];
 
@@ -143,18 +144,12 @@ const readMeQuestions = [
 //function writeToFile(fileName, data) {}
 //Passing 2 arguments to the function  file name and data returned from the promise inquirer.prompt()
 const writeToFile = (fileName, answers) => {
-  return fs.writeFile(fileName, answers, function(err) {
-    if (err)  {
+  return fs.writeFile(fileName, answers, function (err) {
+    if (err) {
       console.log(err);
     }
   });
-
-}
-
-
-
-
-
+};
 
 // TODO: Create a function to initialize app
 const init = () => {
@@ -180,17 +175,13 @@ each section.
 ===========================================================================
     `);
 
-  inquirer.prompt(readMeQuestions)
-    .then((answers) =>  {
-      console.log(answers);
-      console.log('Creating Your professional README');
-      writeToFile('./README.md', generateMarkdown({ ...answers }));
-      // writeToFile('README.md', generateMarkdown({ ...answers }));
-
-    })
-  
+  inquirer.prompt(readMeQuestions).then((answers) => {
+    console.log(answers);
+    console.log("Creating Your professional README");
+    writeToFile("./README.md", generateMarkdown({ ...answers }));
+    // writeToFile('README.md', generateMarkdown({ ...answers }));
+  });
 };
 
 // Function call to initialize app
-init()
-
+init();
